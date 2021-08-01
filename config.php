fecd76af842fde2b9d6eff8fd03663f3172d2d30
alvinotuya84 <alvinotuya8@gmail.com>
2021-08-01 18:16:32 +0300

    changed db name to otuyaTech

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
     $server_name="localhost";
     $db_username="root";
     $db_password="";
-    $db_name="comtech";
+    $db_name="otuyaTech";
     $connect=mysqli_connect($server_name,$db_username,$db_password,$db_name);
     ?>
